2c0641ed748b17ef5003a276d975a2c45dab5dbf
Chris Timperley <christimperley@googlemail.com>
2019-10-12 16:36:05 -0400

    Outlined minimal Shell API (#1)
    * outlined environ
    
    * added EnvNotFoundError
    
    * added execute method
    
    * added imports
    
    * require docker~=4.1.0
    
    * trailing whitespace

--- src/dockerblade/exceptions.py
+++ src/dockerblade/exceptions.py
@@ -4,3 +4,12 @@ import attr as _attr
 
 class DockerBladeException:
     """Used by all exceptions that are thrown by DockerBlade."""
+
+
+@_attr.s(frozen=True, auto_exc=True, auto_attribs=True, str=True)
+class EnvNotFoundError(DockerBladeException):
+    """No environment variable was found with the given name."""
+    name: str
+
+    def __str__(self) -> str:
+        return f"No environment variable found with name: {self.name}"
--- src/dockerblade/shell.py
+++ src/dockerblade/shell.py
@@ -1,6 +1,38 @@
 # -*- coding: utf-8 -*-
 __all__ = ('Shell',)
 
+from typing import Tuple
+
+import attr
+import docker
+
 
 class Shell:
     """Provides shell access to a Docker container."""
+    @staticmethod
+    def for_container(name_or_uid: str) -> 'Shell':
+        # get low-level API
+        # get Docker client
+        # determine container PID
+        raise NotImplementedError
+
+    def environ(self, var: str) -> str:
+        """Reads the value of a given environment variable inside this shell.
+
+        Raises
+        ------
+        EnvNotFoundError
+            if no environment variable exists with the given name.
+        """
+        raise NotImplementedError
+
+    def execute(self, command: str) -> Tuple[int, str, float]:
+        """Executes a given command and blocks until its completion.
+
+        Returns
+        -------
+        Tuple[int, str, float]
+            The return code, output, and wall-clock running time of the
+            execution, measured in seconds.
+        """
+        raise NotImplementedError
