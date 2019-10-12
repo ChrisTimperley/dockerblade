62eb52ce47abb0e8a4cf5c7c97b634f828f6afeb
ChrisTimperley <christimperley@googlemail.com>
2019-10-12 17:48:40 -0400

    added first draft

---
+++ src/dockerblade/stopwatch.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+__all__ = ('Stopwatch',)
+
+from timeit import default_timer as timer
+import warnings
+
+import attr
+
+
+@attr.s(eq=True)
+class Stopwatch:
+    """Used to record the duration of events.
+
+    Attributes
+    ----------
+    paused: bool
+        Indicates whether or not this stopwatch is paused.
+    duration: float
+        The number of seconds that the stopwatch has been running.
+    """
+    _offset: float = attr.ib(default=0.0)
+    _paused: bool = attr.ib(default=True)
+    _time_start: float = attr.ib(default=0.0)
+
+    def stop(self) -> None:
+        """Freezes the stopwatch."""
+        if not self._paused:
+            self._offset += timer() - self._time_start
+            self._paused = True
+
+    def start(self) -> None:
+        """Resumes the stopwatch."""
+        if self._paused:
+            self._time_start = timer()
+            self._paused = False
+        else:
+            warnings.warn("timer is already running")
+
+    @property
+    def paused(self) -> bool:
+        return self._paused
+
+    @property
+    def duration(self) -> float:
+        d = self._offset
+        if not self._paused:
+            d += timer() - self._time_start
+        return d
