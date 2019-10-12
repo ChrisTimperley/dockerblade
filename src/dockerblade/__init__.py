43619510c55faaaed98ce97cc07cff6392016e66
ChrisTimperley <christimperley@googlemail.com>
2019-10-12 16:09:08 -0400

    added shell module

--- src/dockerblade/__init__.py
+++ src/dockerblade/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
 from .files import FileSystem
+from .shell import Shell
---
+++ src/dockerblade/shell.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+__all__ = ('Shell',)
+
+
+class Shell:
+    """Provides shell access to a Docker container."""
