2fb57e267b6e14a8b690a946316153ce6318f8a1
ChrisTimperley <christimperley@googlemail.com>
2019-10-12 16:17:24 -0400

    outlined environ

--- src/dockerblade/shell.py
+++ src/dockerblade/shell.py
@@ -4,3 +4,13 @@ __all__ = ('Shell',)
 
 class Shell:
     """Provides shell access to a Docker container."""
+
+    def environ(self, var: str) -> str:
+        """Reads the value of a given environment variable inside this shell.
+
+        Raises
+        ------
+        EnvNotFoundError
+            if no environment variable exists with the given name.
+        """
+        raise NotImplementedError
