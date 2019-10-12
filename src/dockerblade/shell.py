68dbf1963290e9303feea0f09ca07e3a34c3e9ba
ChrisTimperley <christimperley@googlemail.com>
2019-10-12 16:30:36 -0400

    added imports

--- src/dockerblade/shell.py
+++ src/dockerblade/shell.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 __all__ = ('Shell',)
 
+from typing import Tuple
+
+import attr
+import docker
+
 
 class Shell:
     """Provides shell access to a Docker container."""
+    @staticmethod
+    def for_container(name_or_uid: str) -> 'Shell':
+        # get low-level API
+        # get Docker client
+        # determine container PID
+        raise NotImplementedError
 
     def environ(self, var: str) -> str:
         """Reads the value of a given environment variable inside this shell.
