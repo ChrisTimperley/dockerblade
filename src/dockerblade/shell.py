2156ca774be7efab194e112c25b4db19c7399403
ChrisTimperley <christimperley@googlemail.com>
2019-10-12 16:25:12 -0400

    added execute method

--- src/dockerblade/shell.py
+++ src/dockerblade/shell.py
@@ -14,3 +14,14 @@ class Shell:
             if no environment variable exists with the given name.
         """
         raise NotImplementedError
+
+    def execute(self, command: str) -> Tuple[int, str, float]:
+        """Executes a given command and blocks until its completion.
+
+        Returns
+        -------
+        Tuple[int, str, float]
+            The return code, output, and wall-clock running time of the
+            execution, measured in seconds.
+        """ 
+        raise NotImplementedError
