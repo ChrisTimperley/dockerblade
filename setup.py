d6066c19b851924a3d5ed4a535dd68abe62a0a53
ChrisTimperley <christimperley@googlemail.com>
2019-10-12 16:07:54 -0400

    single source version

--- setup.py
+++ setup.py
@@ -1,2 +1,11 @@
 # -*- coding: utf-8 -*-
-import setuptools; setuptools.setup()
+import os
+import setuptools
+
+PACKAGE = 'dockerblade'
+PATH = os.path.join(os.path.dirname(__file__),
+                    'src/{}/version.py'.format(PACKAGE))
+with open(PATH, 'r') as fh:
+    exec(fh.read())
+
+setuptools.setup(version=__version__)
