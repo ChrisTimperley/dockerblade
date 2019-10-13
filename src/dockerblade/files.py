4ad94fd9db507352375060cced9853e4f5187f6d
ChrisTimperley <christimperley@googlemail.com>
2019-10-13 11:41:54 -0400

    added daemon to filesystem

--- src/dockerblade/files.py
+++ src/dockerblade/files.py
@@ -2,5 +2,10 @@
 __all__ = ('FileSystem',)
 
 
+from .daemon import DockerDaemon
+
+
+@attr.s(eq=False, hash=False)
 class FileSystem:
     """Provides access to a Docker filesystem."""
+    daemon: DockerDaemon = attr.ib(factory=DockerDaemon)
