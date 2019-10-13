a6b32f9d669df564a39962d404c5f3949073e41f
ChrisTimperley <christimperley@googlemail.com>
2019-10-13 11:46:53 -0400

    added container_name property and __repr__

--- src/dockerblade/files.py
+++ src/dockerblade/files.py
@@ -2,10 +2,25 @@
 __all__ = ('FileSystem',)
 
 
+from .shell import Shell
 from .daemon import DockerDaemon
 
 
-@attr.s(eq=False, hash=False)
+@attr.s(slots=True, eq=False, hash=False, repr=False)
 class FileSystem:
-    """Provides access to a Docker filesystem."""
-    daemon: DockerDaemon = attr.ib(factory=DockerDaemon)
+    """Provides access to a Docker filesystem.
+    
+    Attributes
+    ----------
+    container_name: str
+        The name of the container to which the filesystem belongs.
+    """
+    _daemon: DockerDaemon = attr.ib(factory=DockerDaemon)
+    _shell: Shell = attr.ib(init=False)
+
+    @property
+    def container_name(self) -> str:
+        return self._shell.container_name
+
+    def __repr__(self) -> str:
+        return f'FileSystem(container_name={self.container_name})'
