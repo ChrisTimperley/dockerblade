04d504bd7d2c2117074ec338909439e07c18c219
ChrisTimperley <christimperley@googlemail.com>
2019-10-13 11:06:38 -0400

    added post-init

--- src/dockerblade/daemon.py
+++ src/dockerblade/daemon.py
@@ -8,8 +8,14 @@ import docker
 @attr.s(frozen=True)
 class DockerDaemon:
     """Maintains a connection to a Docker daemon."""
-    url: str = attr.ib()
+    url: str = attr.ib(default='unix://var/run/docker.sock')
     client: docker.DockerClient = \
         attr.ib(init=False, eq=False, cmp=False, repr=False)
     api: docker.APIClient = \
         attr.ib(init=False, eq=False, cmp=False, repr=False)
+
+    def __attrs_post_init__(self) -> None:
+        docker_api = docker.APIClient(self.url)
+        docker_client = docker.DockerClient(self.url)
+        object.__setattr__(self, 'client', client)
+        object.__setattr__(self, 'api', api)
