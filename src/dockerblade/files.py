73d331e67819b7d751a94559158f3a2943cccab8
Chris Timperley <christimperley@googlemail.com>
2019-10-19 14:46:34 -0400

    Implemented basic filesystem API (#27)
    * added daemon to filesystem
    
    * added container_name property and __repr__
    
    * excess whitespace
    
    * added exists method
    
    * simpler implementation
    
    * added comment to container_name
    
    * bug fix: missing import
    
    * use str type annotations
    
    * simplified test_shell
    
    * added helper module for testing
    
    * added test_files
    
    * added extra assertions to test_exists
    
    * removed excess whitespace

--- src/dockerblade/files.py
+++ src/dockerblade/files.py
@@ -1,6 +1,31 @@
 # -*- coding: utf-8 -*-
 __all__ = ('FileSystem',)
 
+import typing
+import shlex
 
+import attr
+
+if typing.TYPE_CHECKING:
+    from .shell import Shell
+    from .container import Container
+
+
+@attr.s(slots=True)
 class FileSystem:
-    """Provides access to a Docker filesystem."""
+    """Provides access to a Docker filesystem.
+
+    Attributes
+    ----------
+    container: Container
+        The container to which this filesystem belongs.
+    """
+    container: 'Container' = attr.ib()
+    _shell: 'Shell' = attr.ib(repr=False, eq=False, hash=False)
+
+    def exists(self, path: str) -> bool:
+        """Determines whether a file or directory exists at the given path.
+        Inspired by :meth:`os.path.exists`.
+        """
+        cmd = f'test -e {shlex.quote(path)}'
+        return self._shell.run(cmd, stdout=False).returncode == 0
---
+++ test/test_files.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from common import alpine_310
+
+
+def test_exists(alpine_310):
+    files = alpine_310.filesystem()
+    assert files.exists('/bin/sh')
+    assert files.exists('/bin/cp')
+    assert not files.exists('/bin/foobar')
