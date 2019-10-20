ef4ac0a2309c3b48719a670d951944419148daaa
ChrisTimperley <christimperley@googlemail.com>
2019-10-20 16:20:10 -0400

    added test_isdir

--- test/test_files.py
+++ test/test_files.py
@@ -9,3 +9,9 @@ def test_exists(alpine_310):
     assert files.exists('/bin/sh')
     assert files.exists('/bin/cp')
     assert not files.exists('/bin/foobar')
+
+
+def test_isdir(alpine_310):
+    files = alpine_310.filesystem()
+    assert not files.isdir('/bin/sh')
+    assert files.isdir('/bin')
