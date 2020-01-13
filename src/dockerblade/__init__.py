295cab28c3353ae533cbea42c5ad15115d225a0c
ChrisTimperley <christimperley@googlemail.com>
2020-01-13 13:03:20 +0800

    disable stdout logging by default

--- src/dockerblade/__init__.py
+++ src/dockerblade/__init__.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+from loguru import logger as _logger
+
 from . import exceptions
 from .container import Container
 from .daemon import DockerDaemon
 from .files import FileSystem
 from .shell import Shell, CompletedProcess, CalledProcessError
 from .stopwatch import Stopwatch
+
+_logger.disable('dockerblade')
